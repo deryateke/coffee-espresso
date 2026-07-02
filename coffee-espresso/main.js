af1d8873a5441117d9e0b49f87cdc3ae7fc8e171
deryateke <deryatekeuk@gmail.com>
2026-07-02 12:49:27 +0100

    first commit

---
+++ coffee-espresso/main.js
@@ -0,0 +1,7 @@
+// Html'deki elemanları elemanlara eriş
+const nav = document.querySelector("nav");
+const menuBtn = document.querySelector("#menu-btn");
+
+menuBtn.addEventListener("click", () => {
+  nav.classList.toggle("active");
+});
